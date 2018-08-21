d9a9669ebdcc724315c69d05f4089a032e3cee4f
John Doe <johndoe@example.com>
2018-08-21 22:34:09 +0800

    homework2

---
+++ homework2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace calhomework
+{
+    [TestClass]
+    public partial class TestForm1
+    {
+        [TestMethod]
+        private void Test ()
+        {
+           var result = calhomework.Form1.button5_Click(src,)
+
+        }
+    }
+}
